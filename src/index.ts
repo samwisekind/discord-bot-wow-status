54a4810965bd8f1daf792eaf695001e8ae4165b4
Flamov <sam@flamov.com>
2020-09-30 16:11:35 +0100

    Add comment

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,4 @@ import postResetMessage from './reset';
 
 process.env.TZ = 'UTC';
 
-cron.schedule('0 15 * * *', postResetMessage);
+cron.schedule('0 15 * * *', postResetMessage); // Every day at 15:00 UTC
