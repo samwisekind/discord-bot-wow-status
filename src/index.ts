c13dad6f41673241eddd06bfe5a5132d198d5375
Flamov <sam@flamov.com>
2020-09-30 16:09:23 +0100

    Change from Lambda to cron app

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,9 @@
-import postDiscordChannelMessage from './discord';
+/* istanbul ignore file */
 
-import {
-  RESET_EVENT,
-  RESET_MESSAGES,
-  RESET_WEEKLY_DAY,
-} from './constants';
+import cron from 'node-cron';
+
+import postResetMessage from './reset';
 
 process.env.TZ = 'UTC';
 
-/**
- * Lambda handler function
- * @param {Object} data Lambda event object
- * @param {Object} _context Lambda context object
- * @param {Object} callback Lambda callback function
- * @param {Number} day Current day (as Date number)
- */
-const handler = async (
-  data: { type: String } | null,
-  _context: Object | null,
-  callback: Function,
-  day: Number = new Date().getDay(),
-) => {
-  if (data?.type === RESET_EVENT) {
-    let message = RESET_MESSAGES.DAILY;
-    if (day === RESET_WEEKLY_DAY) {
-      message = RESET_MESSAGES.WEEKLY;
-    }
-
-    await postDiscordChannelMessage(message);
-  }
-
-  return callback(null);
-};
-
-exports.handler = handler;
-
-export { handler };
+cron.schedule('0 15 * * *', postResetMessage);
