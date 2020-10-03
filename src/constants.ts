c429b21f0d6f21c589192a93d2e07b5271c1b513
Sam K <sam@flamov.com>
2020-10-03 01:19:58 +0000

    Remove unused constant

--- src/constants.ts
+++ src/constants.ts
@@ -2,8 +2,6 @@ const DISCORD_CHANNEL_API = 'https://discord.com/api/channels/{CHANNEL_ID}/messa
 
 const DISCORD_AUTHORIZATION_HEADER = 'Bot {TOKEN}';
 
-const RESET_EVENT = 'reset-event';
-
 const RESET_MESSAGES = {
   DAILY: ':tada: Daily quests have reset',
   WEEKLY: ':tada: Daily quests and raids have been reset',
@@ -14,7 +12,6 @@ const RESET_WEEKLY_DAY = 2; // Tuesday
 export {
   DISCORD_CHANNEL_API,
   DISCORD_AUTHORIZATION_HEADER,
-  RESET_EVENT,
   RESET_MESSAGES,
   RESET_WEEKLY_DAY,
 };
